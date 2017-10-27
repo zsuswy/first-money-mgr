2256778e4f2b0fa9eb85a607768fb1d0d6edb946
Jeffrey Sun <zsuswy@gmail.com>
2017-10-27 18:57:04 +0800

    完成单维度测评

--- src/app/model/SurveyDimensionExtraSettings.ts
+++ src/app/model/SurveyDimensionExtraSettings.ts
@@ -1,13 +1,13 @@
 export class SurveyDimensionExtraSettings {
-    constructor(public isShowSumChart?: number,        // 是否显示汇总图
-                public isShowSubChart?: number,        // 是否显示维度的图
+    constructor(public isShowSumChart?: number,         // 是否显示汇总图
+                public isShowSubChart?: number,         // 是否显示维度的图
                 public sumChartType?: number,           // 汇总图类型
                 public sumChartTitle?: string,          // 汇总图标题
                 public sumChartFootScript?: string,     // 汇总图注脚
                 public subChartType?: number,           // 子维度图类型
                 public subChartTitle?: string,          // 子维度图标题
                 public subChartFootScript?: string,     // 子维度图注脚
-                public subDimensionLimitCnt?: number // 显示几个子维度
-    ) {
+                public subDimensionLimitCnt?: number,   // 显示几个子维度
+                public dimensionMaxValue?: number) {    // 维度最大值
     }
 }
